d62d8d7785313d116daf93e220814b0adc44f552
Dijango Alves <dijango_ilha@hotmail.com>
2023-09-22 19:38:20 -0300

    fix: fix console error

--- src/app/(authenticated)/user/layout.jsx
+++ src/app/(authenticated)/user/layout.jsx
@@ -3,19 +3,21 @@ import Header from "@/components/Header"
 import '../../globals.css'
 
 export const metadata = {
-    title: 'Coinc',
-    description: 'Sla',
-  }
-  
-  export default function RootLayout({ children }) {
-    return (
-      <html lang="en">
-        <body className="w-screen flex bg-main">
-            <SideMenu/>
-            <div className="flex flex-col w-full">
-              {children}
-            </div>
-        </body>
-      </html>
-    )
-  }
+  title: 'Coinc',
+  description: 'Sla',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        <div className="w-screen flex bg-main">
+          <SideMenu />
+          <div className="flex flex-col w-full">
+            {children}
+          </div>
+        </div>
+      </body>
+    </html>
+  )
+}
