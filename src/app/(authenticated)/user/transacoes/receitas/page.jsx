9860593fe2b67b86f4ed1e3a623f95fa891f593b
Geovanny N Silva <geovanny.gns1@gmail.com>
2023-10-05 20:35:58 -0300

    fix: remove Header of receitas and despesas

--- src/app/(authenticated)/user/transacoes/receitas/page.jsx
+++ src/app/(authenticated)/user/transacoes/receitas/page.jsx
@@ -2,8 +2,7 @@ import Header from "@/components/Header"
 
 const Receitas = () => {
     return (
-      <>
-        <Header title="Receitas" avatar="https://avatars.githubusercontent.com/u/102611166?s=70&v=4"/> 
+      <> 
         <div>Receitas</div>
       </>
     )
