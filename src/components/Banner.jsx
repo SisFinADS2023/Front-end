776111007abb0e8602c252516cdfc5ff2373ac10
ana788 <anabolinhu@outlook.com>
2023-10-06 19:45:07 -0300

    Ajuste dos tamanhos do layout

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -6,12 +6,12 @@ import Logo from '../../public/logo.svg'
 const Banner = () => {
   return (
     <div id="sidebar-tela-login" className="relative h-screen w-[710px] bg-gradient-to-t from-[#E9F1FB] rounded-br-[50px]">
-      <Image src={Logo} className='absolute top-[36px] left-[92px] w-[178px] h-[52px]' />
-      <p className='absolute top-[200px] left-[92px] text-[45px] font-medium text-4xl max-w-[620px]'>Gerencie suas movimentações financeiras <span className='font-bold'>com facilidade<span className='text-[#11D07D]'>.</span></span></p>
-      <div className='absolute top-[600px] left-[80px] flex flex-col gap-5'>
-        <div className='bg-white h-[79px] w-[505px] rounded-md'></div>
-        <div className='bg-white h-[79px] w-[505px] rounded-md'></div>
-        <div className='bg-white h-[79px] w-[505px] rounded-md'></div>
+      <Image src={Logo} className='absolute top-[60px] left-[90px] w-[295px] h-[78px]' />
+      <p className='absolute top-[331px] left-[90px] text-[45px] font-normal max-w-[620px] text-secondary-500'>Gerencie suas movimentações financeiras <span className='font-bold'>com facilidade<span className='text-[#11D07D] font-bold'>.</span></span></p>
+      <div className='absolute top-[612px] left-[92px] flex flex-col gap-5'>
+        <div className='bg-white h-[79px] w-[505px] rounded-lg shadow-shadowSuave'></div>
+        <div className='bg-white h-[79px] w-[505px] rounded-lg shadow-shadowSuave'></div>
+        <div className='bg-white h-[79px] w-[505px] rounded-lg shadow-shadowSuave'></div>
       </div>
 
     </div>
