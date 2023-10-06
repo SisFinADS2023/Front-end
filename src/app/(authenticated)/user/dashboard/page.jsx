f4f92b621678e164db9e375d0aa8574479a52e46
Geovanny N Silva <geovanny.gns1@gmail.com>
2023-10-05 22:36:39 -0300

    feat: adding the option to view values

--- src/app/(authenticated)/user/dashboard/page.jsx
+++ src/app/(authenticated)/user/dashboard/page.jsx
@@ -1,17 +1,38 @@
 'use client'
 
-import Header from "@/components/Header"
 import Link from 'next/link'
 import Chart from 'chart.js/auto'
 import { useEffect, useState, useRef } from "react"
+import {
+  Eye,
+  EyeSlash,
+} from 'iconsax-react'
 
 const Overview = ({ title, value, path }) => {
+
+  const [visible, setVisible] = useState(true)
+
   return (
     <>
       <Link href={`${path}`}>
-        <div className="flex-start flex-col py-4 px-6 rounded-2xl border-zinc-100 bg-white shadow-md">
+        <div className="flex-start flex-col relative py-4 px-6 rounded-2xl border-zinc-100 bg-white shadow-md overflow-hidden">
           <h4 className="text-sm mb-4 font-semibold">{title}</h4>
-          <span className="text-3xl font-normal">R${value}</span>
+          <div className="flex items-center">
+            {
+              visible ? (
+                <>
+                  <span className="text-3xl font-normal">R${value}</span>
+                  <Eye className="absolute right-[18px] bottom-[13px] text-secondary-200" size={24} onClick={() => setVisible(!visible)}/>
+                </>
+              ) : (
+                <>
+                  <div className="flex items-center h-[36px] text-center"><span className="w-[105px] bg-secondary-500 h-[5px] text-center rounded-full"></span></div>
+                  <EyeSlash className="absolute right-[18px] bottom-[13px] text-secondary-200" size={24} onClick={() => setVisible(!visible)}/>
+                </>
+              )
+            }
+            
+          </div>
         </div>
       </Link>
     </>
@@ -75,14 +96,13 @@ const Dashboard = () => {
         <div className="m-8">
 
           <div className="flex-start flex-col">
-            <h2 className="mb-5 font-normal text-xl">Dashboards</h2>
             <h3 className="font-bold text-5xl mb-3">Overview</h3>
           </div>
 
           <div className="grid grid-cols-4 gap-4">
             <Overview title="Saldo Atual" value="0,00" path="#" />
-            <Overview title="Receitas" value="0,00" path="/tansacoes/receitas" />
-            <Overview title="Despesas" value="0,00" path="/tansacoes/receitas" />
+            <Overview title="Receitas" value="0,00" path="/user/transacoes/receitas" />
+            <Overview title="Despesas" value="0,00" path="/user/transacoes/despesas" />
             <Overview title="Cartão de Crédito" value="0,00" path="#" />
           </div>
 
--- src/app/(authenticated)/user/transacoes/despesas/page.jsx
+++ src/app/(authenticated)/user/transacoes/despesas/page.jsx
@@ -2,8 +2,7 @@ import Header from "@/components/Header"
 
 const Despesas = () => {
     return (
-      <>
-        <Header title="Despesas" avatar="https://avatars.githubusercontent.com/u/102611166?s=70&v=4"/> 
+      <> 
         <div>Despesas</div>
       </>
     )
