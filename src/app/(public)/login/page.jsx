775823ef7660b8f1b9510bf8a6c92748b8c00d7c
ana788 <anabolinhu@outlook.com>
2023-10-26 22:02:13 -0300

    Tela de Recuperar Senha e botão de mostrar/ocultar senha

--- src/app/(public)/login/page.jsx
+++ src/app/(public)/login/page.jsx
@@ -1,5 +1,4 @@
-import Link from "next/link"
-import Image from 'next/image'
+import InputSenha from "@/components/InputSenha"
 
 const Login = () => {
     return (
@@ -7,10 +6,10 @@ const Login = () => {
             <div className='flex flex-col w-[492px] h-[514.87px] px-0 pt-[53px] pb-[32px]'>
                 <h2 className="text-3xl	font-bold text-secondary-500 mb-6">Acesse sua conta</h2>
                 <input className="block mb-6 border-solid border-2 border-gray-900 border-opacity-10 text-[#A2A3A4] pl-2 font-normal text-base h-[48px] rounded" type="text" name='E-mail' placeholder="E-mail"></input>
-                <input className="mb-6 border-solid border-2 border-gray-900 border-opacity-10 text-[#A2A3A4] px-2 font-normal text-base h-[48px] rounded bg-[url(https://cdn-icons-png.flaticon.com/512/829/829117.png)] bg-no-repeat bg-[length:20px_20px] bg-[calc(100%-10px)]" type="text" name='Senha' placeholder="Senha"></input>
+                {/* <input className="mb-6 border-solid border-2 border-gray-900 border-opacity-10 text-[#A2A3A4] px-2 font-normal text-base h-[48px] rounded bg-[url(https://cdn-icons-png.flaticon.com/512/829/829117.png)] bg-no-repeat bg-[length:20px_20px] bg-[calc(100%-10px)]" type="text" name='Senha' placeholder="Senha"></input> */}
+                <InputSenha />
 
-
-                <p className='mb-6 text-base font-semibold text-secondary-500'><a href='#'>Esqueceu a senha? ➜</a></p>
+                <p className='mb-6 text-base font-semibold text-secondary-500'><a href='../recuperar-senha'>Esqueceu a senha? ➜</a></p>
                 <button className='mb-3 bg-secondary-500 rounded text-white h-[45px] text-base font-bold'>Entrar</button>
                 <p className='mb-3 flex justify-start text-base font-light text-secondary-500'>Não possui uma conta? <a href='../cadastro' className="text-primary-500 font-semibold ml-1">Cadastre-se agora.</a></p>
 
