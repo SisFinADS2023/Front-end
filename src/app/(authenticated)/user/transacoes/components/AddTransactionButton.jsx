79dcf7d2ad7740d71e4d16b77104d354377512f0
gilsonsilven <gilsonsilven@gmail.com>
2023-11-10 21:04:53 -0300

    style: change add transaction button color

--- src/app/(authenticated)/user/transacoes/components/AddTransactionButton.jsx
+++ src/app/(authenticated)/user/transacoes/components/AddTransactionButton.jsx
@@ -7,7 +7,7 @@ const AddTransactionButton = () => {
 
     return (
         <div className="w-[80px] h-[80px]">
-            <button className="w-[80px] h-[80px] bg-[#11D07D] rounded-full flex justify-center items-center">
+            <button className="w-[80px] h-[80px] bg-primary-500 rounded-full flex justify-center items-center">
                 <Add size="64" color="#FFFFFF"/>
             </button>
         </div>
