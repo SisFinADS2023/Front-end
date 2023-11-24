a9b5c13491826d92897657d97b6984f712d9b33f
agathasoaresx <agatacsoares@gmail.com>
2023-11-23 21:45:56 -0300

    atualização form cadastro

--- src/app/(public)/cadastro/page.jsx
+++ src/app/(public)/cadastro/page.jsx
@@ -30,7 +30,7 @@ const Cadastro = () => {
   return (
     <>
     
-    <form className="flex justify-center items-center w-[1210px]">
+    <div className="flex justify-center items-center w-[1210px]">
 
       <div id="cadastro-card" className="flex flex-col h-auto w-[496px] justify-center">
         <div id="cadastro-title" className="h-14 ">
@@ -38,6 +38,8 @@ const Cadastro = () => {
           <h2 className="font-sans text-3xl league_Spartan">Crie sua conta</h2>
           </div>
         </div>
+
+      <form>
         <div id="cadastro-body" className="w-auto">
           <div id="nome-sobrenome" className="flex flex space-x-4 ">
             <input type="text" name="nome" placeholder="Nome" className="border-2 text-[#A2A3A4] px-2 font-sans text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10 "></input>
@@ -52,6 +54,7 @@ const Cadastro = () => {
             <InputConfSenha/>
             </div>
           </div>
+       
 
           <div id="termos" className="h-16 pt-3 flex ">
             <input type="checkbox"/><p className="flex text-[14px] align-middle pl-4 pt-4 text-left text-[#979797]">Eu li e aceito a política de privacidade do sistema, concordando em compartilhar meus dados conforme descrito nos termos estabelecidos. </p>
@@ -59,16 +62,18 @@ const Cadastro = () => {
 
           <div id="input" className="pt-9">
           <div className={inter.className}>
-            <button id="buttonSignUp" className="rounded-md w-[496px] h-[45px] bg-lightGray font-sans text-[#979797]" >Cadastre-se</button>
+            <button id="buttonSignUp"  type="form" className="rounded-md w-[496px] h-[45px] bg-lightGray font-sans text-[#979797]" >Cadastre-se</button>
           </div>
           </div>
         </div>
+      </form>
+
         <div className="flex pt-3 items-center justify-center">
           <p className="text-center">Já possui uma conta?</p><a href="../login" className="pl-2 text-green font-sans font-bold">Fazer Login</a>
         </div>
       </div>
   
-    </form>
+    </div>
     </>
   )
 }
