a013a185af1a35a28e542931e86787814647ef0f
agathasoaresx <agatacsoares@gmail.com>
2023-11-23 21:22:02 -0300

    layout e form cadastro atualizado

--- src/app/(public)/cadastro/page.jsx
+++ src/app/(public)/cadastro/page.jsx
@@ -1,6 +1,9 @@
+'use client'
 import React from 'react'
 import {League_Spartan} from 'next/font/google'
 import { Inter } from 'next/font/google'
+import {useState} from 'react'
+
 
 import InputSenha from '@/components/InputSenha'
 import InputConfSenha from '@/components/InputConfSenha'
@@ -21,72 +24,51 @@ const inter = Inter(
   }
 )
 
-const ButtonHandle  = () => {
-    document.getElementById("buttonSignUp").disabled = true;
-
-    document.getElementById("input[type='checkbox']").addEventListener("input[type='checkbox']", function(event){
-
-      //busca conteúdo do input
-        var conteudo = document.getElementById("input[type='checkbox']").value;
-    
-        //valida conteudo do input 
-        if (conteudo !== null && conteudo !== '') {
-          //habilita o botão
-          document.getElementById("buttonSignUp").disabled = false;
-        } else {
-          //desabilita o botão se o conteúdo do input ficar em branco
-          document.getElementById("buttonSignUp").disabled = true;
-        }
-    });
-
-}
-
-
 const Cadastro = () => {
-
 
 
   return (
     <>
     
-    <div class="flex justify-center items-center w-[1210px]">
-      <div id="cadastro-card" class="flex flex-col h-auto w-[496px] justify-center">
-        <div id="cadastro-title" class="h-14 ">
+    <form className="flex justify-center items-center w-[1210px]">
+
+      <div id="cadastro-card" className="flex flex-col h-auto w-[496px] justify-center">
+        <div id="cadastro-title" className="h-14 ">
           <div className={league_Spartan.className}>
-          <h2 class="font-sans text-3xl league_Spartan">Crie sua conta</h2>
+          <h2 className="font-sans text-3xl league_Spartan">Crie sua conta</h2>
           </div>
         </div>
-        <div id="cadastro-body" class="w-auto">
-          <div id="nome-sobrenome" class="flex flex space-x-4 ">
-            <input type="text" id="name" name="name" value="" placeholder="Nome" class="border-2 text-[#A2A3A4] px-2 font-sans text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10 "></input>
-            <input type="text" id="name" name="sobrenome" value="" placeholder="Sobrenome" class="border-2 text-[#A2A3A4] px-2 font-normal text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10"></input> 
+        <div id="cadastro-body" className="w-auto">
+          <div id="nome-sobrenome" className="flex flex space-x-4 ">
+            <input type="text" name="nome" placeholder="Nome" className="border-2 text-[#A2A3A4] px-2 font-sans text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10 "></input>
+            <input type="text" name="sobrenome" placeholder="Sobrenome" className="border-2 text-[#A2A3A4] px-2 font-normal text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10"></input> 
           </div>
 
-          <div id="form-content" class="flex flex-col space-y-6 pt-6">
-            <input type="text" id="name" name="cpf" value="" placeholder="Username" class="border-2 text-[#A2A3A4] px-2 font-sans text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10"></input>
-            <input type="text" id="name" name="email" value="" placeholder="E-mail" class="border-2 text-[#A2A3A4] px-2 font-sans text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10"></input>
-            <input type="text" id="name" name="email" value="" placeholder="Confirmar E-mail" class="border-2 text-[#A2A3A4] px-2 font-sans text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10"></input>
+          <div id="form-content" className="flex flex-col space-y-6 pt-6">
+            <input type="text" name="email" placeholder="E-mail" className="border-2 text-[#A2A3A4] px-2 font-sans text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10"></input>
+            <input type="text" name="email" placeholder="Confirmar E-mail" className="border-2 text-[#A2A3A4] px-2 font-sans text-base h-[48px] rounded w-[100%] border-gray-900 border-opacity-10"></input>
             <div className="h-[126px]">
             <InputSenha/>
             <InputConfSenha/>
             </div>
           </div>
 
-          <div id="termos" class="h-16 pt-3 flex ">
-            <input type="checkbox" /><p class="flex text-[14px] align-middle pl-4 pt-4 text-left text-[#979797]">Eu li e aceito a política de privacidade do sistema, concordando em compartilhar meus dados conforme descrito nos termos estabelecidos. </p>
+          <div id="termos" className="h-16 pt-3 flex ">
+            <input type="checkbox"/><p className="flex text-[14px] align-middle pl-4 pt-4 text-left text-[#979797]">Eu li e aceito a política de privacidade do sistema, concordando em compartilhar meus dados conforme descrito nos termos estabelecidos. </p>
           </div>
 
-          <div id="input" class="pt-9">
+          <div id="input" className="pt-9">
           <div className={inter.className}>
-            <button id="buttonSignUp"  className="rounded-md w-[496px] h-[45px] bg-lightGray font-sans text-[#979797] ">Cadastre-se</button>
+            <button id="buttonSignUp" className="rounded-md w-[496px] h-[45px] bg-lightGray font-sans text-[#979797]" >Cadastre-se</button>
           </div>
           </div>
         </div>
-        <div class="flex pt-3 items-center justify-center">
-          <p className="text-center">Já possui uma conta?</p><a href="../login" class="pl-2 text-green font-sans font-bold">Fazer Login</a>
+        <div className="flex pt-3 items-center justify-center">
+          <p className="text-center">Já possui uma conta?</p><a href="../login" className="pl-2 text-green font-sans font-bold">Fazer Login</a>
         </div>
       </div>
-    </div>
+  
+    </form>
     </>
   )
 }
--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,17 +1,31 @@
 import React from 'react'
 import Image from 'next/image'
 import Logo from '../../public/logo.svg'
+import Analyst from '../../public/financial-analyst.svg'
+import Frame1 from '../../public/Frame1.svg'
+import Frame2 from '../../public/Frame2.svg'
+import Frame3 from '../../public/Frame3.svg'
 
 
 const Banner = () => {
   return (
     <div id="sidebar-tela-login" className="relative h-screen w-[710px] bg-gradient-to-t from-[#E9F1FB] rounded-br-[50px]">
-      <Image src={Logo} className='absolute top-[60px] left-[90px] w-[295px] h-[78px]' />
-      <p className='absolute top-[331px] left-[90px] text-[45px] font-normal max-w-[620px] text-secondary-500'>Gerencie suas movimentações financeiras <span className='font-bold'>com facilidade<span className='text-[#11D07D] font-bold'>.</span></span></p>
-      <div className='absolute top-[612px] left-[92px] flex flex-col gap-5'>
-        <div className='bg-white h-[79px] w-[505px] rounded-lg shadow-shadowSuave'></div>
-        <div className='bg-white h-[79px] w-[505px] rounded-lg shadow-shadowSuave'></div>
-        <div className='bg-white h-[79px] w-[505px] rounded-lg shadow-shadowSuave'></div>
+      <div className='absolute top-[60px] left-[90px]'><Image src={Logo} className=' w-[295px] h-[78px]' /></div>
+      <div className='absolute top-[300px] left-[90px] right-[20px]'>
+        <p className=' text-[42px] font-normal max-w-[620px] text-secondary-500'>
+          Gerencie suas movimentações financeiras 
+          <span className='font-bold'> com facilidade<span className='text-[#11D07D] font-bold'>.</span></span>
+          </p>
+      </div>
+      <div className='absolute relative top-[500px] left-[80px] h-[500px] w-[500px]'>
+
+          <Image src={Frame1} className='h-auto w-auto ' />
+       
+          <Image src={Frame2} className='h-auto w-auto' />
+        
+          <Image src={Frame3} className='h-auto w-auto' />
+       
+
       </div>
 
     </div>
