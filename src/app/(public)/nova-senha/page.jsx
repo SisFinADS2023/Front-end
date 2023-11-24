56392c5056cad3aec16b3dda714feea6a870d0a8
agathasoaresx <agatacsoares@gmail.com>
2023-11-23 22:00:35 -0300

    tela nova senha atualizada

--- src/app/(public)/nova-senha/page.jsx
+++ src/app/(public)/nova-senha/page.jsx
@@ -22,32 +22,34 @@ const inter = Inter(
 
 const NovaSenha = () => {
   return (
-    <div class="flex justify-center items-center w-[1210px]">
-      <div id="novaSenha-card" class="flex flex-col h-auto w-[496px] justify-center">
-        <div id="novaSenha-title" class="h-[36px]">
+    <div className="flex justify-center items-center w-[1210px]">
+      <div id="novaSenha-card" className="flex flex-col h-auto w-[496px] justify-center">
+        <div id="novaSenha-title" className="h-[36px]">
           <div className={league_Spartan.className}>
-          <h2 class="font-sans text-3xl league_Spartan text-secondary-500">Nova Senha</h2>
+          <h2 className="font-sans text-3xl league_Spartan text-secondary-500">Nova Senha</h2>
           </div>
         </div>
-        <div id="novaSenha-body" class="w-auto h-auto">
+        <div id="novaSenha-body" className="w-auto h-auto">
 
-          <div id="form-content" class="flex flex-col space-y-[30px] pt-6">
+          <div id="form-content" className="flex flex-col space-y-[30px] pt-6">
           <div className="h-[126px]">
+            <form>
            <InputSenha/>
            <InputConfSenha/>
+           </form>
            </div>
           </div>
 
-          <div id="input" class="pt-[23px]">
+          <div id="input" className="pt-[23px]">
           <div className={inter.className}>
-            <button class="rounded-md w-[496px] h-[45px] bg-secondary-500 font-sans text-secondary-50">Salvar</button>
+            <button type="form" className="rounded-md w-[496px] h-[45px] bg-secondary-500 font-sans text-secondary-50">Salvar</button>
           </div>
           </div>
         </div>
 
-        <div id="politicas" class="h-[74px] pt-[21px] flex flex-col items-center">
-           <div class='flex'><p class="flex text-[14px] pl-4 text-[#979797]">Politicas de privacidade</p></div>
-           <div class='flex'><p class="flex text-[14px] pl-4 pt-3 text-[#979797]">Direitos autorais © 2023 XXXX. Todos os direitos reservados.</p></div>
+        <div id="politicas" className="h-[74px] pt-[21px] flex flex-col items-center">
+           <div className='flex'><p className="flex text-[14px] pl-4 text-[#979797]">Politicas de privacidade</p></div>
+           <div className='flex'><p className="flex text-[14px] pl-4 pt-3 text-[#979797]">Direitos autorais © 2023 XXXX. Todos os direitos reservados.</p></div>
         </div>
       </div>
     </div>
