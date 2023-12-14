c08c357d62929fc55b15801beb7424acc5fed42f
unknown <nojiri.keithi@aluno.ifsp.edu.br>
2023-12-14 17:19:23 -0300

    fix: sei la input senha

--- src/components/inputSenha.jsx
+++ src/components/inputSenha.jsx
@@ -6,29 +6,29 @@ import {
     EyeSlash
 } from 'iconsax-react'
 
-const InputSenha = ({ onChange }) => {
 
+const InputSenha = ({placeholder}) => {
     const [isVisible, setIsVisible] = useState(false)
 
-
     const visible = () => {
+        console.log(isVisible)
         setIsVisible(!isVisible)
+
+
     }
 
     return (
         <>
             <div className="relative">
-                <input
-                    onChange={onChange}
-                    className={`mb-6 border-solid border-2 border-gray-900 border-opacity-10 text-[#A2A3A4] px-2 font-normal text-base h-[48px] rounded w-[100%]`}
-                    type={isVisible ? "text" : "password"} name='Senha' placeholder="Senha">
-                </input>
+                <input id="password" className={`mb-6 border-solid border-2 border-gray-900 border-opacity-10 text-[#A2A3A4] px-2 font-normal text-base h-[48px] rounded w-[100%]`}
+                    type={isVisible ? "text" : "password"} name='Senha' placeholder={placeholder}></input>
                 {
                     isVisible
                         ? <EyeSlash size={20} onClick={visible} className="absolute cursor-pointer top-[15px] right-[11px] text-[#A2A3A4]" />
                         : <Eye size={20} onClick={visible} className="absolute cursor-pointer top-[15px] right-[11px] text-[#A2A3A4]" />
                 }
             </div>
+
 
         </>
     )
