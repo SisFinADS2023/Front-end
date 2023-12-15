c55f3f8d18e98d19e4b009211fb645d2d06e1b28
rogerkeithi <nojiri.keithi@aluno.ifsp.edu.br>
2023-12-15 01:44:57 -0300

    fix: url

--- src/components/ContaAndCards/Utils/ListBankAccounts.jsx
+++ src/components/ContaAndCards/Utils/ListBankAccounts.jsx
@@ -11,7 +11,7 @@ const ListBankAccounts = () => {
     //This is where the GET endpoint should be.
     useEffect(() => {
         const getAccounts = async () => {
-            const response = await fetch('https://cgtnvhork6.execute-api.us-east-1.amazonaws.com/dev/bank-accounts/user/1234567890', {cache: 'no-store', mode: 'no-cors'})
+            const response = await fetch('https://cm6skfzcne.execute-api.us-east-1.amazonaws.com/dev/bank-accounts/user/1234567890', {cache: 'no-store', mode: 'no-cors'})
             const data = await response.json()
             setAccounts(data.bankAccounts)
         }
