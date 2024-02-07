af858e98768c3e513aa3d864f4bf4b23fc43817f
Roger Keithi Nojiri da Silva <110481359+rogerkeithi@users.noreply.github.com>
2024-02-07 16:03:17 -0300

    fix: update layout
    remove unusual text

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 export const metadata = {
   title: 'Coinc',
-  description: 'Sla',
+  description: 'Junte-se ao COINC para uma jornada de controle, compreensão e crescimento financeiro. Comece hoje e libere o potencial dos seus dados financeiros!',
 }
 
 export default function RootLayout({ children }) {
